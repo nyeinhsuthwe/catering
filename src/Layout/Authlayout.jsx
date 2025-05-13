eea1b68e62a9772190e958be977a11ebe1bf46a5
nyeinhsuthwe <nyeinhsuthwe57@gmail.com>
2025-05-13 16:49:48 +0630

    update layout

--- src/Layout/Authlayout.jsx
+++ src/Layout/Authlayout.jsx
@@ -2,10 +2,10 @@ import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
   return (
-    <>
+    <div>
         <Outlet/>
-    </>
+    </div>
   )
 }
 
-export default AuthLayout;
+export default AuthLayout
